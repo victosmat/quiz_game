04855d005a00fcb5cc8e741374b5c2e36b385fa0
victosmat <74144091+victosmat@users.noreply.github.com>
2022-06-14 14:31:48 +0700

    push

--- funtion/funtionDangKy.js
+++ funtion/funtionDangKy.js
@@ -1,21 +1,44 @@
-const $ = document.querySelector.bind(document);
 
-const button_password = $("#password");
-const button_re_password = $("#re_password");
-const button_submit = $("#submit");
-
-function ktraDangKy() {
-    if (button_password.value != button_re_password.value) {
-        button_password.style.border = '5px solid red ';
-        button_re_password.style.border = '5px solid red ';
-    } else {
-        window.location.href = "DangNhap.html";
-    }
+function kiemtratendangnhap(tendangnhap) {
+    var kiemtra = /^[a-zA-Z0-9]{4,32}$/;
+    return kiemtra.test(tendangnhap);
 }
 
-document.addEventListener("keypress", (e) => {
-    if (e.key == "Enter") {
-        ktraDangKy();
+function dangKy() {
+    var ten = document.getElementById("ten").value;
+    var tendangnhap = document.getElementById("tendangnhap").value;
+    var matkhau = document.getElementById("matkhau").value;
+    var matkhau2 = document.getElementById("matkhau2").value;
+    if (ten == "" || tendangnhap == "" || matkhau == "" || matkhau2 == "") {
+        alert("Vui lòng nhập đầy đủ thông tin!");
     }
-})
+    else if (matkhau != matkhau2) {
+        alert("Mật khẩu không trùng khớp!");
+    }
+    else if (!kiemtratendangnhap(tendangnhap)) {
+        alert("Tên đăng nhập có độ dài từ 4 đến 30 ký tự và chỉ chứa các chữ cái và số");
+    }
+    else {
+        var fetch = new Fetch();
+        fetch.post("dangky", {
+            ten: ten,
+            tendangnhap: tendangnhap,
+            matkhau: matkhau
+        })
+            .then(function (data) {
+                if (data.success) {
+                    alert("Đăng ký thành công!");
+                    var token = data.token;
+                    localStorage.setItem("token", token);
+                    window.location.href = "/";
+                } else {
+                    alert(data.message);
+                }
+            })
+            .catch(function (err) {
+                alert("Đã có lỗi xảy ra!");
+                console.log(err);
+            });
+    }
+}
 
